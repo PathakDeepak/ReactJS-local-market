cf9eab158e3edfcfa45683d544e5460f4e9e766c
deepak pathak <pathakdeepak024@gmail.com>
2021-06-21 10:03:06 +0530

    created sign-in page and styling

---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './sign-in-and-sign-up.styles.scss';
+
+import SignIn from '../../components/sign-in/sign-in.component';
+
+export const SignInSignUpPage = () => (
+    <div className='sign-in-and-sign-up'><SignIn/></div>
+);
